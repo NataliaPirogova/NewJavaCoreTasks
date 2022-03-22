451456a2489ad014d1f44f045043355a5a6dd52f
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-22 21:39:02 +0300

    done Task3

--- src/Pirogova/Lesson5/Task3.java
+++ src/Pirogova/Lesson5/Task3.java
@@ -6,9 +6,15 @@ public class Task3 {
     public static void main(String[] args) {
         int []a = new int[15];
         Random r = new Random();
+        int count = 0; //кол-во четных элементов
         for (int i = 0; i < 15; i++) {
-            a[i] = r.nextInt(99);
+            a[i] = r.nextInt(100);
             System.out.print(a[i] + " ");
+            if(a[i]%2==0){
+                count++;
+            }
         }
+        System.out.println();
+        System.out.println("Количество четных элементов в массиве: " + count);
     }
 }
