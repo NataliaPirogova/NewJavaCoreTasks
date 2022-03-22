39a52ecbaf6d3588401c8de24018b2f30179dd5e
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-22 19:49:15 +0300

    done Task8

--- src/Pirogova/Lesson5/Task8.java
+++ src/Pirogova/Lesson5/Task8.java
@@ -1,4 +1,33 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Task8 {
+    public static void main(String[] args) {
+        int[]a = new int[10];
+        int[]b =new int[10];
+        double[]c = new double[10];
+        Random r = new Random();
+        for(int i=0;i<10;i++){
+            a[i]=r.nextInt(9);
+            b[i]=r.nextInt(9);
+            if(b[i]==0){
+                c[i]=0;
+            }
+            else{
+                c[i]=a[i] / b[i];
+            }
+        }
+        System.out.print("Первый массив: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(a[i] + " ");}
+        System.out.println();
+        System.out.print("Второй массив: ");
+            for (int i = 0; i < 10; i++) {
+                System.out.print(b[i] + " ");}
+        System.out.println();
+        System.out.print("Третий массив: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(c[i] + " ");}
+    }
 }
