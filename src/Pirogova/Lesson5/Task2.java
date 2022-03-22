7b3b45a00067ad7879645d9df616af197c8ca718
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-22 21:23:57 +0300

    Task2 mass 1-99

--- src/Pirogova/Lesson5/Task2.java
+++ src/Pirogova/Lesson5/Task2.java
@@ -2,6 +2,16 @@ package Pirogova.Lesson5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        int count = 0;
+        for (int a = 1; a <= 99; a+=2){
+            count++;
+        }
+        int []mass = new int[count];
+        int b = 1;
+        for (int i = 0; i < count; i++) {
+            mass[i] = b;
+            b+=2;
+            System.out.print(mass[i] + " ");
+        }
     }
 }
