2834c13e5e5611246e549bd476a7bac3a2683faf
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-22 21:35:16 +0300

    Task3 mass

--- src/Pirogova/Lesson5/Task3.java
+++ src/Pirogova/Lesson5/Task3.java
@@ -1,4 +1,14 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Task3 {
+    public static void main(String[] args) {
+        int []a = new int[15];
+        Random r = new Random();
+        for (int i = 0; i < 15; i++) {
+            a[i] = r.nextInt(99);
+            System.out.print(a[i] + " ");
+        }
+    }
 }
