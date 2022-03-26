efdc1828ee6d791e9bd76d61930c7a4a440f75d9
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-26 22:25:47 +0300

    done Extra2

--- src/Pirogova/Lesson5/Extra2.java
+++ src/Pirogova/Lesson5/Extra2.java
@@ -1,4 +1,24 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Extra2 {
+    public static void main(String[] args) {
+        int[] mass = new int[10];
+        Random r = new Random();
+        double sum = 0;//сумма нечетных элементов
+        double average;//среднее арифметическое
+        int count = 0;//количество нечетных элементов
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = r.nextInt(10);
+            System.out.print(mass[i] + " ");
+            if(i%2!=0){
+                count++;
+                sum+=mass[i];
+            }
+        }
+        System.out.println();
+        average = sum/count;
+        System.out.println("Среднее арифметическое нечетных элементов: " + average);
+    }
 }
