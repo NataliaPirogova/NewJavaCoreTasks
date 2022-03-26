1313cce1a9d3037e17a0a15dc30a413226b0df04
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-26 13:48:40 +0300

    Task5 create massives

--- src/Pirogova/Lesson5/Task5.java
+++ src/Pirogova/Lesson5/Task5.java
@@ -1,4 +1,21 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Task5 {
+    public static void main(String[] args) {
+        int []mass1 =new int[5];
+        int []mass2 =new int[5];
+        Random r = new Random();
+        for (int i = 0; i < mass1.length; i++) {
+            mass1[i] = r.nextInt(15);
+            System.out.print(mass1[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < mass2.length; i++) {
+            mass2[i] = r.nextInt(15);
+            System.out.print(mass2[i] + " ");
+        }
+
+    }
 }
