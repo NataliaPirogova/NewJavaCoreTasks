179a1a9cb4a61204329e736c95c967ccb815256b
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-26 21:08:13 +0300

    v1 Task6

--- src/Pirogova/Lesson5/Task6.java
+++ src/Pirogova/Lesson5/Task6.java
@@ -1,4 +1,24 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Task6 {
+    public static void main(String[] args) {
+        int []mass = new int[4];
+        Random ra = new Random();
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = ra.nextInt(10);
+            System.out.print(mass[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < mass.length; i++) {
+            if(mass[i]<mass[i+1]){
+               continue;
+            }
+            else{
+                System.out.println("Нет строго возрастающей последовательности");
+                break;
+            }
+        }
+    }
 }
