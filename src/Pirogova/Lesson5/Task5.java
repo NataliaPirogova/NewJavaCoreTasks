8630f77a6826f3a3827f91b77a4fef31b65afcdc
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-26 13:58:51 +0300

    done Task5 v1

--- src/Pirogova/Lesson5/Task5.java
+++ src/Pirogova/Lesson5/Task5.java
@@ -16,6 +16,29 @@ public class Task5 {
             mass2[i] = r.nextInt(15);
             System.out.print(mass2[i] + " ");
         }
-
+        System.out.println();
+        double average1;//среднее арифметическое первого массива
+        double average2;//среднее арифметическое второго массива
+        double summ1 = 0;//сумма элементов первого массива
+        double summ2 = 0;//сумма элементов первого массива
+        for (int i = 0; i < mass1.length; i++) {
+            summ1+=mass1[i];
+            }
+        average1 = summ1/ mass1.length;
+        System.out.println("Среднее арифметическое элементов первого массива: " + average1);
+        for (int i = 0; i < mass2.length; i++) {
+            summ2+=mass2[i];
+        }
+        average2 = summ2/ mass2.length;
+        System.out.println("Среднее арифметическое элементов второго массива: " + average2);
+        if(average1>average2){
+            System.out.println("Среднее арифметическое первого массива больше");
+        }
+        if(average1<average2){
+            System.out.println("Среднее арифметическое второго массива больше");
+        }
+        if(average1==average2){
+            System.out.println("Средние арифметические массивов равны");
+        }
     }
 }
