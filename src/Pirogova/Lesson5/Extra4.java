31727c0a8a06c57063accedf56f99c4617a05a84
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-26 22:49:53 +0300

    done Extra4

--- src/Pirogova/Lesson5/Extra4.java
+++ src/Pirogova/Lesson5/Extra4.java
@@ -1,4 +1,23 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Extra4 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int n = r.nextInt(20);//кол-во элементов массива
+        int[] mass = new int[n];
+        for (int i = 0; i < n; i++) {
+            mass[i] = r.nextInt(50);
+            System.out.print(mass[i] + " ");
+        }
+        System.out.println();
+        int min = mass[0];
+        for (int i = 0; i < n; i++) {
+            if(i%2!=0 && mass[i]<min){
+                min = mass[i];
+            }
+        }
+        System.out.println("Наименьший нечетный элемент: " + min);
+    }
 }
