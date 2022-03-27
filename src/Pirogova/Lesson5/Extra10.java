ae8d31d880f946d6cfd6ffb31b68a4b9981c73ca
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-27 21:04:52 +0300

    done Extra10

--- src/Pirogova/Lesson5/Extra10.java
+++ src/Pirogova/Lesson5/Extra10.java
@@ -1,4 +1,30 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Extra10 {
+    public static void main(String[] args) {
+        int[] mass = new int[26];
+        Random r = new Random();
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = r.nextInt(1995);
+            System.out.print(mass[i] + " ");
+        }
+        System.out.println();
+        int max = mass[0];
+        for (int i = 0; i < mass.length; i++) {
+            if(mass[i]>max){
+                max = mass[i];
+            }
+        }
+        System.out.println("Максимальный элемент: " + max);
+        int value = mass[0];
+        for (int i = 0; i < mass.length; i++) {
+            if(mass[i]==max){
+                mass[i]=value;
+            }
+            mass[0] = max;
+            System.out.print(mass[i] + " ");
+        }
+    }
 }
