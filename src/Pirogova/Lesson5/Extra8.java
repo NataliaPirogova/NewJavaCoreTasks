7c3f09339ca2ec1039e4d2177c647ddbc866e704
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-27 20:49:46 +0300

    done Extra8

--- src/Pirogova/Lesson5/Extra8.java
+++ src/Pirogova/Lesson5/Extra8.java
@@ -1,4 +1,29 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Extra8 {
+    public static void main(String[] args) {
+        int[] mass = new int[10];
+        Random r = new Random();
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = r.nextInt(40);
+            System.out.print(mass[i] + " ");
+        }
+        System.out.println();
+        int max = mass[0];
+        for (int i = 0; i < mass.length; i++) {
+            if(mass[i]>max){
+                max = mass[i];
+            }
+        }
+        System.out.println("Максимальное значение: " + max);
+        int max2 = 0;
+        for (int i = 0; i < mass.length; i++) {
+            if(mass[i]>max2 && mass[i]<max){
+                max2 = mass[i];
+            }
+        }
+        System.out.println("Второе значение по величине: " + max2);
+    }
 }
