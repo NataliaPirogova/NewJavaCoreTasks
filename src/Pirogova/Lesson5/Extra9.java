df10dd70b0b24e781dec98c4b70589972f03d54d
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-27 20:55:14 +0300

    start Extra9

--- src/Pirogova/Lesson5/Extra9.java
+++ src/Pirogova/Lesson5/Extra9.java
@@ -1,4 +1,22 @@
 package Pirogova.Lesson5;
 
+import java.util.Random;
+
 public class Extra9 {
+    public static void main(String[] args) {
+        int[] mass = new int[20];
+        Random r = new Random();
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = r.nextInt(30);
+            System.out.print(mass[i] + " ");
+        }
+        System.out.println();
+        int min = mass[0];
+        for (int i = 0; i < mass.length; i++) {
+            if(mass[i]<min && i%2==0){
+                min = mass[i];
+            }
+        }
+        System.out.println("Минимальный элемент с четным индексом массива: " + min);
+    }
 }
