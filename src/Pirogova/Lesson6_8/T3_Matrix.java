7295d92b1ad6b62c2874b7240de94d242752e19d
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-31 21:45:59 +0300

    Matrix methods

--- src/Pirogova/Lesson6_8/T3_Matrix.java
+++ src/Pirogova/Lesson6_8/T3_Matrix.java
@@ -4,7 +4,23 @@ public class T3_Matrix {
     int a;//количество столбцов
     int b;//количество строк
     double[][] mass = new double[a][b];
-    int value;//значение, на которое будем умножать
 
+public void umnozh (int value){
+    for (int i = 0; i < this.mass.length; i++) {
+        for (int j = 0; j < this.mass.length; j++) {
+            this.mass[a][b]=value*this.mass[a][b];
+            System.out.println(this.mass[a][b] + " ");
+        }
+        System.out.println();
+    }
+}
 
+public void print(){
+    for (int i = 0; i < this.mass.length; i++) {
+        for (int j = 0; j < this.mass.length; j++) {
+            System.out.println(this.mass[a][b] + " ");
+        }
+        System.out.println();
+    }
+}
 }
