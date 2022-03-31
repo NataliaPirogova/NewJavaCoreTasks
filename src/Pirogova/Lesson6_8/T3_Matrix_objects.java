4403acc2a0487a4171db4e966d9bcc9790373fe9
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-31 21:53:20 +0300

    Reader create class

--- src/Pirogova/Lesson6_8/T3_Matrix_objects.java
+++ src/Pirogova/Lesson6_8/T3_Matrix_objects.java
@@ -1,4 +1,11 @@
 package Pirogova.Lesson6_8;
 
 public class T3_Matrix_objects {
+    private String FIO;//ФИО
+    private int numero;//номер читательского билета
+    private String Faculty;//факультет
+    private String DoB;//дата рождения
+    private long phone;//телефон
+
+
 }
