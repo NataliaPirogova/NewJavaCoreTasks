5c5f0394acedf1aef94f8cbd795b034b2ca940ae
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-04-03 00:04:20 +0300

    Reader version2 class

--- src/Pirogova/Lesson6_8/Reader/Reader.java
+++ src/Pirogova/Lesson6_8/Reader/Reader.java
@@ -2,10 +2,16 @@ package Pirogova.Lesson6_8.Reader;
 
 public class Reader {
     private String FIO;//ФИО
-    private int numero;//номер читательского билета
+    private int number;//номер читательского билета
     private String Faculty;//факультет
     private String DoB;//дата рождения
     private long phone;//телефон
 
+    public void takeBook(){
 
+    }
+
+    public void returnBook(){
+
+    }
 }
