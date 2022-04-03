c7b43369560d18db4e726bf2109413a1ff421739
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-04-03 19:29:27 +0300

    Matrix class version3

--- src/Pirogova/Lesson6_8/Matrix/Matrix.java
+++ src/Pirogova/Lesson6_8/Matrix/Matrix.java
@@ -1,5 +1,7 @@
 package Pirogova.Lesson6_8.Matrix;
 
+import java.util.Random;
+
 public class Matrix {
     private int a;//количество строк
     private int b;//количество столбцов
@@ -8,20 +10,30 @@ public class Matrix {
     public Matrix() {
     }
 
+    public Matrix(int a, int b){
+        int [][]mass = new int[a][b];
+        Random r = new Random();
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                mass[i][j]=r.nextInt(10);
+            }
+        }
+    }
+
     public void multiply (int value){//value - значение, на которое умножаем
-    for (int i = 0; i < this.mass.length; i++) {
-        for (int j = 0; j < this.mass.length; j++) {
-            this.mass[a][b]=value*this.mass[a][b];
-            System.out.println(this.mass[a][b] + " ");
+    for (int i = 0; i < this.a; i++) {
+        for (int j = 0; j < this.b; j++) {
+            this.mass[i][j]=value*this.mass[i][j];
+            System.out.println(this.mass[i][j] + " ");
         }
         System.out.println();
     }
 }
 
 public void print(){
-    for (int i = 0; i < this.mass.length; i++) {
-        for (int j = 0; j < this.mass.length; j++) {
-            System.out.println(this.mass[a][b] + " ");
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+            System.out.print(mass[i][j] + " ");
         }
         System.out.println();
     }
--- src/Pirogova/Lesson6_8/Matrix/Matrix_objects.java
+++ src/Pirogova/Lesson6_8/Matrix/Matrix_objects.java
@@ -1,4 +1,8 @@
 package Pirogova.Lesson6_8.Matrix;
 
 public class Matrix_objects {
+   public static void main(String[] args) {
+      Matrix Matrix1 = new Matrix(3,3);
+      Matrix1.print();
+   }
    }
