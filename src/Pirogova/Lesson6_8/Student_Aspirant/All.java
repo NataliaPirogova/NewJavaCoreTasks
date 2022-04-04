64f518ad32306b7f3e17321128284d902a9a7c6a
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-04-04 22:55:43 +0300

    done Student

---
+++ src/Pirogova/Lesson6_8/Student_Aspirant/All.java
@@ -0,0 +1,15 @@
+package Pirogova.Lesson6_8.Student_Aspirant;
+
+public class All {
+    public static void main(String[] args) {
+        Student S1 = new Aspirant();
+        Student[] students = new Student[3];
+        students[0] = new Student("Igor", "Ivanov", "group17", 3);
+        students[1] = new Student("Oksana", "Vaskina", "group7", 5);
+        students[2] = new Student("Kirill", "Teterev", "group21", 3.9);
+
+        for (Student student: students) {
+            student.getScholarship(student);
+        }
+    }
+}
