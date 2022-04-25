29c26e5d526aeed8e6935bad24d0711e38a2bfe6
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-04-25 22:34:14 +0300

    Polindrom v2

--- src/Pirogova/Lesson11/Polindrom/Polindrom.java
+++ src/Pirogova/Lesson11/Polindrom/Polindrom.java
@@ -1,13 +1,16 @@
 package Pirogova.Lesson11.Polindrom;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Polindrom {
     public static void main(String[] args) {
-        FileReader fileIn = null;
+        BufferedReader BfR = null;
+        File PoliWords = new File("D:\\NewJavaCoreTasks\\src\\Pirogova\\Lesson11\\Polindrom\\PoliWords.txt");
 
-        try {fileIn = new FileReader("original words.txt");
+        try {BfR = new BufferedReader(new FileReader("D:\\NewJavaCoreTasks\\src\\Pirogova\\Lesson11\\Polindrom\\original words.txt"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
