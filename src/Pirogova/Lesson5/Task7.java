9d063df06fb9f7b88aa1b9067e0a6681966de724
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-05-03 16:07:19 +0300

    Task7 correct

--- src/Pirogova/Lesson5/Task7.java
+++ src/Pirogova/Lesson5/Task7.java
@@ -12,15 +12,16 @@ public class Task7 {
         }
         System.out.println();
         int max = 0;
-        for (int i = 0; i < mass.length; i++) {
-            if(mass[i]>max){
-                max = mass[i];
+        for (int j : mass) {
+            if (j > max) {
+                max = j;
             }
         }
         System.out.println("Максимальное число: " + max);
-        for (int i = mass.length-1; i > 0; i--) {
+        for (int i = mass.length-1; i >= 0; i--) {
             if(mass[i]==max){
                 System.out.println("Последний индекс максимального числа: " + i);
+                break;
             }
 
         }
