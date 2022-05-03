f07086e260feb358829c15eaf9c9f3beec3ec1db
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-05-03 16:24:50 +0300

    Task9 correct

--- src/Pirogova/Lesson5/Task9.java
+++ src/Pirogova/Lesson5/Task9.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class Task9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число для определения размера массива");
+        System.out.println("Введите целое число для определения размера массива");
         int size = sc.nextInt();
-        int s_left = 0;
-        int s_right = 0;
+//        int s_left = 0;
+//        int s_right = 0;
         if(size<=0){
             System.out.println("Неподходящее значение!");
         }
@@ -22,19 +22,30 @@ public class Task9 {
                 System.out.print(a[i] + " ");
             }
             System.out.println();
-            for (int i = 0; i <(size/2) ; i++) {
-                s_left = s_left + a[i];
+            int summ=0;
+            for (int i = 0; i < (size/2); i++) {
+                summ=summ+a[size-1-i]-a[i];
             }
-            for (int i = (size/2); i <size; i++) {
-                s_right = s_right + a[i];
-            }
-            if(s_left > s_right){
+            if(summ<0){
                 System.out.println("Сумма левой половины больше");
-            }else if(s_left < s_right){
+            }else if(summ>0){
                 System.out.println("Сумма правой половины больше");
             }else{
-                System.out.println("Обе половины равны");
-            }
+                System.out.println("Обе половины равны");}
+
+            //            for (int i = 0; i <(size/2) ; i++) {
+//                s_left = s_left + a[i];
+//            }
+//            for (int i = (size/2); i <size; i++) {
+//                s_right = s_right + a[i];
+//            }
+//            if(s_left > s_right){
+//                System.out.println("Сумма левой половины больше");
+//            }else if(s_left < s_right){
+//                System.out.println("Сумма правой половины больше");
+//            }else{
+//                System.out.println("Обе половины равны");
+//            }
         }
     }
 }
