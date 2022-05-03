906e623fad4a825b55f604a4725582226395347c
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-05-03 14:10:08 +0300

    done multiplication table

---
+++ src/Pirogova/Lesson4/Task9.java
@@ -0,0 +1,13 @@
+package Pirogova.Lesson4;
+
+public class Task9 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                int a = i*j;
+                System.out.print(i+"*"+j+"="+a+"   ");
+            }
+            System.out.println();
+        }
+    }
+}
