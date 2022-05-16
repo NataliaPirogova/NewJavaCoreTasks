69d09f008d920bff277b23781b7935557445c44f
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-05-17 00:31:26 +0300

    Lesson 14 words v1

--- src/Pirogova/Lesson14/Words.java
+++ src/Pirogova/Lesson14/Words.java
@@ -1,4 +1,23 @@
 package Pirogova.Lesson14;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Words {
+    public static void main(String[] args) {
+        List <String> originWords = new ArrayList<>();
+        Collections.addAll(originWords, "dad", "funny", "pop", "strange", "status");
+
+        List<String> filteredWords = originWords.stream()
+                .filter(s->s.endsWith(String.valueOf(s.charAt(0)))).collect(Collectors.toList());
+
+        for (String s:filteredWords
+             ) {
+            System.out.println(s);
+        }
+
+    }
 }
