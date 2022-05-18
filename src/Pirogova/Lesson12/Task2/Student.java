22623dcd5404d45450160eb2261520160240e801
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-05-18 22:18:39 +0300

    Collection Task 2 v1

--- src/Pirogova/Lesson12/Task2/Student.java
+++ src/Pirogova/Lesson12/Task2/Student.java
@@ -1,4 +1,50 @@
 package Pirogova.Lesson12.Task2;
 
 public class Student {
+    private String name;
+    private int group;
+    private int course;
+    private double mark;
+
+    public Student(String name, int group, int course, double mark) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.mark = mark;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
 }
