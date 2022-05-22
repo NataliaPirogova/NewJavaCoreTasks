aa90051d5173c7fcbe7ed05002170fedcef286b3
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-05-22 19:48:27 +0300

    Lesson 14 Task 5 v1

--- src/Pirogova/Lesson14/Task5/Test.java
+++ src/Pirogova/Lesson14/Task5/Test.java
@@ -1,4 +1,16 @@
 package Pirogova.Lesson14.Task5;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 public class Test {
+    public static void main(String[] args) {
+        Collection collection1 = Arrays.asList("a1","a2","a3","a1");
+        Collection collection2 = Arrays.asList("1,2,0","4,5");
+
+        collection1.stream().map(p->p+"_1").forEach(System.out::println);
+
+        //второе задание?
+
+    }
 }
