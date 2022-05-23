91fb161bd1a2b0689b942b68306b01b78c0221c8
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-05-24 00:26:26 +0300

    Regex Leap year done

--- src/Pirogova/Lesson15_Reg/RegexDemo.java
+++ src/Pirogova/Lesson15_Reg/RegexDemo.java
@@ -11,19 +11,20 @@ public class RegexDemo
             String date = "2001-02-29";
             Pattern pattern = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
             Matcher m = pattern.matcher(date);
-            System.out.println(m.matches());
+            while (m.find()){
+                // System.out.println(m.matches());
 
-            Pattern p29 = Pattern.compile("\\d{4}-\\d{2}-29");
-            Matcher m29 = p29.matcher(date);
-            if (m29.matches()){
-                String year = date.substring(0,4);
-                int year1 = Integer.parseInt(year);
-                System.out.println(year1);
-                if (year1%4==0){
-                    System.out.println("Год високосный, все ок");
-                }
-                else {
-                    System.out.println("Год не високосный, не может быть 29 февраля");
+                Pattern p29 = Pattern.compile("\\d{4}-02-29");
+                Matcher m29 = p29.matcher(date);
+                if (m29.matches()) {
+                    String year = date.substring(0, 4);
+                    int year1 = Integer.parseInt(year);
+                //    System.out.println(year1);
+                    if (year1 % 4 == 0) {
+                        System.out.println("Год високосный, все ок");
+                    } else {
+                        System.out.println("Год не високосный, не может быть 29 февраля");
+                    }
                 }
             }
 
